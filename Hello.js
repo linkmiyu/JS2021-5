a3103474ed3da5ba1633badba15da1de65f17dfe
linkmiyu <dbwlsgus77gmail.com>
2021-03-23 16:51:37 +0900

    03/23  last push

--- Hello.js
+++ Hello.js
@@ -5,3 +5,28 @@ console.log("Hello world!");
 console.log("Hello world!");
 console.log("Hello world!");
 *****************************/
+
+
+console.log (h >= 3 && h <- 8);
+
+let input = 31;
+
+let date = new Date;
+
+
+
+
+if (input %2 == 0) {
+    console.log("짝수");
+}
+ else {
+     console.log("홀수");
+ }
+
+ console.log(date.getFullYear());
+ console.log(date.getMounth());
+ console.log(date.getDay());
+ console.log(date.getTime());
+ console.log(date.getUTCMinutes());
+ console.log(date.getSeconds());
+ console.log(date.getDate());
